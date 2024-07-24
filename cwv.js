e34aa2c39bef2683307d276e9c187b97c2e641a1
Joshua Wilson <joshua.wilson@academy.com>
2024-07-24 13:43:12 -0500

    optimize API

--- cwv.js
+++ cwv.js
@@ -4,28 +4,40 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const path = 'cwv.txt';
-const PORT = 3000;
-const data = fs.existsSync(path) ? JSON.parse(fs.readFileSync(path).toString()) : {};
-
-const addData = dataToAdd => {
-  const keys = Object.keys(data);
-  const id = keys.length;
-  data[id] = { ...dataToAdd, id };
-  fs.writeFileSync(path, JSON.stringify(data, null, 2), err => err && console.error(err));
-};
+const port = 3000;
+const writeStream = fs.existsSync(path) ? fs.createWriteStream(path, { flags: 'a' }) : null;
 
 const app = express();
 
 app.use(cors());
+
 app.post('/', bodyParser.json(), (req, res) => {
   if (req.body) {
-    addData(req.body);
-    console.log(req.body);
-    res.send('data added.');
+    try {
+      writeStream.write(`${JSON.stringify(req.body)}\n`);
+      console.log('data added...', req.body.value);
+    } catch (err) {
+      console.log(err);
+    }
   } else {
-    console.log('empty request body received.')
-    res.send('empty request body received.');
+    res.send('empty request body...');
   }
 });
-app.get('/', (req, res) => res.send(data));
-app.listen(PORT, err => err ? console.log(err) : console.log('Server listening on PORT', PORT));
+
+app.get('/', (req, res) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server listening on port...', port);
+  }
+});
